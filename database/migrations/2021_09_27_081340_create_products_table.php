d2263ff643786e0399967084e7aff0245e7aaff4
Ali Ghanavati <ali.agk25@gmail.com>
2021-09-27 12:18:54 +0330

    add products categories product_galleries migration && add is_admin to users table

---
+++ database/migrations/2021_09_27_081340_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('title',150);
+            $table->text('description');
+            $table->string('image');
+            $table->integer('price');
+            $table->integer('discount');
+
+            $table->integer('quantity')->default(0);
+            $table->integer('sold')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
