88435c4303411e1c84417c1e9443d239deff0786
Ali Ghanavati <ali.agk25@gmail.com>
2021-09-29 18:32:22 +0330

    fix some bug in index view  products and categories

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -39,10 +39,11 @@
                     <div class="text-sm text-gray-900">Iphone 13 Pro Max 512GB Midnight Black</div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
-                    <div class="text-sm text-red-600 line-through">10,000$</div>
+                    <div class="text-sm text-red-600 line-through hidden">10,000$</div>
                     <div class="text-sm text-gray-500">8,000$</div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900 px-2 hidden">15</div>
                     <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-600">
                       Out of stock
                     </span>
@@ -54,29 +55,6 @@
                 </td>
             </tr>
 
-            <tr>
-                <td class="px-4 py-2">
-                    <div class="h-14 w-14">
-                        <img class="h-14 w-14 rounded" src="https://fakeimg.pl/200/" alt="fake img">
-                    </div>
-                </td>
-                <td class="px-6 py-4">
-                    <div class="text-sm text-gray-900">PlayStation 5 Console</div>
-                </td>
-                <td class="px-6 py-4">
-                    <div class="text-sm">1,000$</div>
-                </td>
-                <td class="px-6 py-4">
-                    <div class="text-sm text-gray-900 px-2">15</div>
-                </td>
-                <td class="px-6 py-4 text-left text-sm font-medium space-x-1.5">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Gallery</a>
-                    <a href="#" class="text-yellow-500 hover:text-yellow-900">Edit</a>
-                    <a href="#" class="text-red-600 hover:text-red-900">Delete</a>
-                </td>
-            </tr>
-
-            <!-- More people... -->
             </tbody>
         </table>
     </div>
